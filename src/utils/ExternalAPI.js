cfc48a1a322079bb18efa55606d4e0bd2dc881fd
Charles Reis Ribeiro <charles.reis@townsq.com.br>
2022-04-21 12:44:51 -0300

    Fix linting in components and utils

--- src/utils/ExternalAPI.js
+++ src/utils/ExternalAPI.js
@@ -1,29 +1,27 @@
-const CLIENT_ID = "SOWI0WCWL54SREB4NZ3HG1VZZCVY4TELQQG5V2I1IHRFKDRI";
-const CLIENT_SECRET = "3UZ45VHPQQAOUPKNN4FGJ0BKSOMZM0SKUXHTCCINPSQLM3UJ";
+const CLIENT_ID = 'SOWI0WCWL54SREB4NZ3HG1VZZCVY4TELQQG5V2I1IHRFKDRI';
+const CLIENT_SECRET = '3UZ45VHPQQAOUPKNN4FGJ0BKSOMZM0SKUXHTCCINPSQLM3UJ';
 // const BASE_URL = "https://api.foursquare.com/v2/venues/explore";
-const BASE_URL = "https://api.foursquare.com/v2/venues/search?";
-const API_VERSION = "20190726";
+const BASE_URL = 'https://api.foursquare.com/v2/venues/search?';
+const API_VERSION = '20190726';
 
-export const getPlaces = () =>{
+export const getPlaces = () => {
+  const parameters = {
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    query: 'sushi',
+    near: 'Curitiba',
+    v: API_VERSION,
+  };
 
-    const parameters ={
-        client_id : CLIENT_ID,
-        client_secret : CLIENT_SECRET,
-        query: "sushi",
-        near: "Curitiba",
-        v: API_VERSION
-    }
-
-        return fetch(BASE_URL+new URLSearchParams(parameters))
-        .then(res => {
-            if (res.ok==false) 
-            { throw res }
-        console.log(res.json);
-        return res.json()
-        })
-        .then(data => data.response.venues)
-        // .catch(error => {
-        //     console.error(error)
-        //     window.alert("Foursquare API failed...")
-        // })
-}
+  return fetch(BASE_URL + new URLSearchParams(parameters))
+    .then((res) => {
+      if (res.ok == false) { throw res; }
+      console.log(res.json);
+      return res.json();
+    })
+    .then((data) => data.response.venues);
+  // .catch(error => {
+  //     console.error(error)
+  //     window.alert("Foursquare API failed...")
+  // })
+};
